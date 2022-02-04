0898fca3fd049e0a97657095cec0e39c9b56c316
novalramdhani <novalfitrahramdhani@gmail.com>
2022-02-05 05:11:28 +0700

    add new files

---
+++ rubydasar/comment.rb
@@ -0,0 +1,7 @@
+# puts string with uppercase word
+puts "hello im noval".upcase 
+
+=begin
+this is 
+    multiple comment
+end=
---
+++ rubydasar/variable.rb
@@ -0,0 +1,9 @@
+my_string = "Hello Ruby!"
+
+my_number = 100
+
+my_boolean = true
+
+my_null = nil
+
+puts my_string
