2be15e8cb764f1e9937fc4e84fa0932a31865230
novalramdhani <novalfitrahramdhani@gmail.com>
2022-03-04 06:02:10 +0700

    wip

---
+++ rubydasar/operators.rb
@@ -0,0 +1,12 @@
+puts 2 == 2 && 2 == 2 # and
+
+puts 9 > 10 || 11 < 10 # or
+
+puts !true # not
+
+puts !false # not
+
+problem = false
+print "Good to go!" unless problem
+
+# puts (x && (y || w)) && z # combining boolean operator
